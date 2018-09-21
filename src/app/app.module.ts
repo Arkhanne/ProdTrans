4ba47fa5e59d20ff5bf4cc3d631c377dcd3d0f2f
Edu Benito <eduardbenito@icloud.com>
2018-09-21 13:42:36 +0200

    add product-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 const routes: Routes = [
   { path: '',  component: TransactionsPageComponent },
+  { path: 'product/:id',  component: ProductPageComponent },
   // { path: '**', component: E404PageComponent }
   ];
 
@@ -21,6 +23,7 @@ const routes: Routes = [
     TransactionsPageComponent,
     ProductsListComponent,
     ProductCardComponent,
+    ProductPageComponent,
   ],
   imports: [
     BrowserModule,
